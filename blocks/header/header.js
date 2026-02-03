332f0677ffe41ee765df7294e9f16ae59d344451
Deepa Prabhu <dprabhu@adobe.com>
2026-02-03 12:02:23 +0530

    header changes

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,6 +1,5 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
-console.log(nav)
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
 
